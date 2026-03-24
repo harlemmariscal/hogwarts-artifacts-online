c1b330d187757427f12d5350c118ba5c553436db
Harlem Mariscal <harlem.mariscal@tcu.edu>
2026-03-23 21:40:49 -0500

    find all artifacts implemation

--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -29,8 +31,25 @@ class ArtifactServiceTest {
     @InjectMocks
     ArtifactService artifactService;
 
+    List<Artifact> artifacts;
+
     @BeforeEach
     void setUp() {
+        Artifact a1 = new Artifact();
+        a1.setId("1250808601744904191");
+        a1.setName("Deluminator");
+        a1.setDescription("A Deluminator is a device invented by Albus Dumbledore that resembles a cigarette lighter. It is used to remove or absorb (as well as return) the light from any light source to provide cover to the user.");
+        a1.setImageUrl("imageUrl");
+
+        Artifact a2 = new Artifact();
+        a2.setId("1250808601744904192");
+        a2.setName("Invisibility Cloak");
+        a2.setDescription("An invisibility cloak is used to make the wearer invisible.");
+        a2.setImageUrl("imageUrl");
+
+        this.artifacts = new ArrayList<>();
+        this.artifacts.add(a1);
+        this.artifacts.add(a2);
     }
 
     @AfterEach
@@ -83,6 +102,23 @@ class ArtifactServiceTest {
                 .isInstanceOf(ArtifactNotFoundException.class)
                 .hasMessage("Could not find artifact with ID 1250808601744904192");
                 verify(artifactRepository, times(1)).findById("1250808601744904192");
+    }
+
+    @Test
+    void testFindAllSuccess() {
+        // given
+        given(artifactRepository.findAll()).willReturn(this.artifacts);
+
+
+
+        // when
+        List<Artifact> actualArtifacts = artifactService.findAll();
+
+
+
+        // then
+        assertThat(actualArtifacts.size()).isEqualTo(this.artifacts.size());
+        verify(artifactRepository, times(1)).findAll();
     }
 
 }
