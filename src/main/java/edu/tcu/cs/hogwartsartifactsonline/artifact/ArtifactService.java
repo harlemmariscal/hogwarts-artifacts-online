7e9c24024f91dbd8753cab301d628cbe7edb0030
Harlem Mariscal <harlem.mariscal@tcu.edu>
2026-03-23 22:30:29 -0500

    Add an Artifact

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactService.java
@@ -1,5 +1,6 @@
 package edu.tcu.cs.hogwartsartifactsonline.artifact;
 
+import edu.tcu.cs.hogwartsartifactsonline.artifact.utils.IdWorker;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,12 @@ public class ArtifactService {
 
     private final ArtifactRepository artifactRepository;
 
+    private final IdWorker idWorker;
 
-    public ArtifactService(ArtifactRepository artifactRepository) {
+
+    public ArtifactService(ArtifactRepository artifactRepository, IdWorker idWorker) {
         this.artifactRepository = artifactRepository;
+        this.idWorker = idWorker;
     }
 
     public Artifact findById(String artifactId) {
@@ -25,5 +29,9 @@ public class ArtifactService {
         return this.artifactRepository.findAll();
     }
 
+    public Artifact save(Artifact newArtifact) {
+        newArtifact.setId(idWorker.nextId() + "");
+        return this.artifactRepository.save(newArtifact);
+    }
 
 }
--- src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactControllerTest.java
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactControllerTest.java
@@ -1,9 +1,11 @@
 package edu.tcu.cs.hogwartsartifactsonline.artifact;
 
+import edu.tcu.cs.hogwartsartifactsonline.artifact.dto.ArtifactDto;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
@@ -30,6 +34,9 @@ class ArtifactControllerTest {
 
     @MockitoBean
     ArtifactService artifactService;
+
+    @Autowired
+        ObjectMapper objectMapper;
 
     List<Artifact> artifacts;
 
@@ -127,6 +134,36 @@ class ArtifactControllerTest {
                 .andExpect(jsonPath("$.data[0].name").value("Deluminator"))
                 .andExpect(jsonPath("$.data[1].id").value("1250808601744904192"))
                 .andExpect(jsonPath("$.data[1].name").value("Invisibility Cloak"));
+    }
+
+    @Test
+    void testAddArtifactSuccess() throws Exception {
+        // Given
+        ArtifactDto artifactDto = new ArtifactDto(null,
+                "Remembrall",
+                "A Remembrall was a magical large marble-sized glass ball that contained smoke which turned red when its owner or user had forgotten something. It turned clear once whatever was forgotten was remembered.",
+                "ImageUrl",
+                null);
+        String json = this.objectMapper.writeValueAsString(artifactDto);
+
+        Artifact savedArtifact = new Artifact();
+        savedArtifact.setId("1250808601744904197");
+        savedArtifact.setName("Remembrall");
+        savedArtifact.setDescription("A Remembrall was a magical large marble-sized glass ball that contained smoke which turned red when its owner or user had forgotten something. It turned clear once whatever was forgotten was remembered.");
+        savedArtifact.setImageUrl("ImageUrl");
+
+        given(this.artifactService.save(Mockito.any(Artifact.class))).willReturn(savedArtifact);
+
+
+        // When and then
+        this.mockMvc.perform(post("/api/v1/artifacts").contentType(MediaType.APPLICATION_JSON).content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.flag").value(true))
+                .andExpect(jsonPath("$.code").value(StatusCode.SUCCESS))
+                .andExpect(jsonPath("$.message").value("Add Success"))
+                .andExpect(jsonPath("$.data.id").isNotEmpty())
+                .andExpect(jsonPath("$.data.name").value(savedArtifact.getName()))
+                .andExpect(jsonPath("$.data.description").value(savedArtifact.getDescription()))
+                .andExpect(jsonPath("$.data.imageUrl").value(savedArtifact.getImageUrl()));
     }
 
 }
